fba74a0504a58affd8516c8357f6d6d0f329c0b8
shagejack.eth <shagejack@outlook.com>
2022-01-18 00:44:08 +0800

    Update ModSetup.java

--- src/main/java/shagejack/industrimania/registers/setup/ModSetup.java
+++ src/main/java/shagejack/industrimania/registers/setup/ModSetup.java
@@ -4,7 +4,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import shagejack.industrimania.Industrimania;
+import shagejack.industrimania.commands.CommandRemoveRocks;
 import shagejack.industrimania.content.worldGen.GenerationRegistry;
+import shagejack.industrimania.registers.AllCommands;
 
 @Mod.EventBusSubscriber(modid = Industrimania.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModSetup {
@@ -12,7 +14,7 @@ public class ModSetup {
     public static void setup() {
         IEventBus bus = MinecraftForge.EVENT_BUS;
         bus.addListener(GenerationRegistry::onBiomesLoaded);
-
+        bus.addListener(AllCommands::registerCommand);
 
     }
 
